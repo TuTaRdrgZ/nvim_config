5bd524702defa56440e70759a62530cb2a64b963
Bautista Rodriguez <bautirodri@gmail.com>
2024-12-23 23:43:11 +0100

    tuqui

--- init.lua
+++ init.lua
@@ -5,16 +5,16 @@
 --]]
 vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 -- Add lazy to the `runtimepath`, this allows us to `require` it.
@@ -23,7 +23,9 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set up lazy, and load my `lua/tuta/plugins/` folder
 require("lazy").setup({ import = "tuta/plugins" }, {
-  change_detection = {
-    notify = false,
-  },
+	change_detection = {
+		notify = false,
+	},
 })
+
+vim.cmd("colorscheme gruvbuddy")
---
+++ lua/tuta/plugins/snacks.lua
@@ -0,0 +1,88 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			statuscolumn = { enabled = false },
+			quickfile = { enabled = false },
+			rename = { enabled = false },
+			bufdelete = { enabled = false },
+			dashboard = { enabled = true },
+			terminal = {
+				enabled = true,
+				win = {
+					position = "float",
+					border = "single",
+				},
+			},
+			words = {
+				enabled = true,
+				debounce = 200,
+				notify_jump = false,
+				notify_end = true,
+				foldopen = true,
+				jumplist = true,
+				modes = { "n" },
+			},
+		},
+		-- keys = {
+		-- 	{
+		-- 		"<leader>lg",
+		-- 		function()
+		-- 			Snacks.lazygit()
+		-- 		end,
+		-- 		desc = "Lazygit",
+		-- 	},
+		-- 	{
+		-- 		"<leader>gB",
+		-- 		function()
+		-- 			Snacks.gitbrowse()
+		-- 		end,
+		-- 		desc = "Git Browse",
+		-- 	},
+		-- 	{
+		-- 		"<leader>gf",
+		-- 		function()
+		-- 			Snacks.lazygit.log_file()
+		-- 		end,
+		-- 		desc = "Lazygit Current File History",
+		-- 	},
+		-- 	{
+		-- 		"<leader>gl",
+		-- 		function()
+		-- 			Snacks.lazygit.log()
+		-- 		end,
+		-- 		desc = "Lazygit Log (cwd)",
+		-- 	},
+		-- 	{
+		-- 		"<c-t>",
+		-- 		function()
+		-- 			Snacks.terminal.toggle()
+		-- 		end,
+		-- 		desc = "Toggle Terminal",
+		-- 	},
+		-- 	{
+		-- 		"]]",
+		-- 		function()
+		-- 			Snacks.words.jump(vim.v.count1)
+		-- 		end,
+		-- 		desc = "Next Reference",
+		-- 	},
+		-- 	{
+		-- 		"[[",
+		-- 		function()
+		-- 			Snacks.words.jump(-vim.v.count1)
+		-- 		end,
+		-- 		desc = "Prev Reference",
+		-- 	},
+		-- },
+		sections = {
+			{ section = "header" },
+			{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+			{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			{ section = "startup" },
+		},
+	},
+}
