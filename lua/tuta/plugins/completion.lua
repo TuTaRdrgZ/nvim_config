7ddb0c66934a8d90f27c1664fed0065531afc6c2
Bautista Rodriguez Ramal <bautrodr@cbr11s1.42barcelona.com>
2025-07-31 13:09:10 +0200

    accept completion with <Tab>

--- lua/tuta/plugins/completion.lua
+++ lua/tuta/plugins/completion.lua
@@ -15,14 +15,14 @@ return {
 		opts = {
 			keymap = {
 				preset = "default",
-				["<Tab>"] = { "snippet_forward", "fallback" },
+				--["<Tab>"] = { "snippet_forward", "fallback" },
 				["<S-Tab>"] = { "snippet_backward", "fallback" },
 
 				["<C-k>"] = { "select_prev", "fallback" },
 				["<C-j>"] = { "select_next", "fallback" },
 				["<C-p>"] = { "select_prev", "fallback" },
 				["<C-n>"] = { "select_next", "fallback" },
-				["<C-enter>"] = { "select_and_accept", "fallback" },
+				["<Tab>"] = { "select_and_accept", "fallback" },
 
 				["<C-b>"] = { "scroll_documentation_up", "fallback" },
 				["<C-f>"] = { "scroll_documentation_down", "fallback" },
